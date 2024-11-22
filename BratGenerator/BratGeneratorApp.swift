022b5967a0241dc60fa8e0ba7bd33b5c127d0bf7
Denis Haritonenko <114603705+zassimouse@users.noreply.github.com>
2024-11-22 21:44:14 +0300

    Initial Commit

---
+++ BratGenerator/BratGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BratGeneratorApp.swift
+//  BratGenerator
+//
+//  Created by Denis Haritonenko on 22.11.24.
+//
+
+import SwiftUI
+
+@main
+struct BratGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BratGenerator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BratGenerator
+//
+//  Created by Denis Haritonenko on 22.11.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
